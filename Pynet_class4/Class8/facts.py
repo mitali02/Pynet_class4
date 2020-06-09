a304b208c53f1c01b9297e465a755916f52144b2
mitali02 <mitali.bhojwani@gmail.com>
2020-06-09 13:06:54 +0530

    Update facts.py

--- Pynet_class4/Class8/facts.py
+++ Pynet_class4/Class8/facts.py
@@ -2,6 +2,6 @@ from jnpr.junos import Device
 from getpass import getpass
 from pprint import pprint
 
-a_device = Device(host="srx2.lasthop.io", user="pyclass", password="88newclass")
+a_device = Device(host="srx2.lasthop.io", user="pyclass", password="")
 a_device.open()
 pprint(a_device.facts['serialnumber'])
